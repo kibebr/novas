3aaeee54d6214198c5d0b17237eb98f8dd0d58e3
kibebr <0763722@students.dadeschools.net>
2021-03-03 03:21:26 +0000

    uses react to detect if there is a query already

--- pages/search/index.js
+++ pages/search/index.js
@@ -3,33 +3,45 @@ import { Footer } from '../../components/Footer.js'
 import { Container } from '../../components/Container.js'
 import { ArticleCard } from '../../components/ArticleCard.js'
 import { useRouter } from 'next/router'
+import { useEffect, useState } from 'react'
 
 export default function Search () {
+  const [foundArticles, setFoundArticles] = useState([])
   const router = useRouter()
   const { query } = router.query
+
+  useEffect(() => {
+    if (query === undefined) {
+      console.warn('no query')
+    }
+
+    console.log('no articles')
+  }, [])
 
   return (
     <div>
       <Header borderColor='border-blue-600' />
       <Container>
-        <div className='mt-10'></div>
+        <div className=''>
+          <div className='mt-10'></div>
 
-        <div className='text-center'>
-          <h1 contentEditable={true} className='font-bold text-7xl'>"Test"</h1>
-          <p className='mt-3 tracking-widest'>SEARCH RESULTS:</p>
+          <div className='text-center'>
+            <input placeholder='Search for articles' className='text-center font-bold text-5xl md:text-7xl' />
+            <p className='mt-3 text-md md:text-lg tracking-widest'>SEARCH RESULTS:</p>
+          </div>
+
+          <ul className='mt-10'>
+            <li className='mb-5'>
+              <ArticleCard />
+            </li>
+            <li className='mb-5'>
+              <ArticleCard />
+            </li>
+            <li>
+              <ArticleCard />
+            </li>
+          </ul>
         </div>
-
-        <ul className='mt-10'>
-          <li className='mb-5'>
-            <ArticleCard />
-          </li>
-          <li className='mb-5'>
-            <ArticleCard />
-          </li>
-          <li>
-            <ArticleCard />
-          </li>
-        </ul>
       </Container>
 
       <div className='mt-10'></div>
