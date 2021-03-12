8c614c845e339b830f7d4996b061ff52f7fa96f7
kibebr <0763722@students.dadeschools.net>
2021-03-12 17:35:41 +0000

    added simple api to search for articles

---
+++ pages/api/articles/[name].ts
@@ -0,0 +1,25 @@
+import { NextApiHandler } from 'next'
+import { Article } from '../../../domain/interfaces'
+import { flow, pipe } from 'fp-ts/function'
+import { values } from 'fp-ts-std/Record'
+import { contains, toLower } from 'fp-ts-std/String'
+import { chain, filter } from 'fp-ts/Array'
+import { prop } from 'fp-ts-ramda'
+import categories from '../../../fetchers/categories.preval'
+
+const handler: NextApiHandler = (req, res) => {
+  const articles: Article[] = pipe(
+    categories,
+    values,
+    chain(prop('articles')),
+    filter(flow(
+      prop('title'),
+      toLower,
+      contains(req.query?.name as string)
+    ))
+  )
+
+  res.status(200).send(articles)
+}
+
+export default handler
