9bf9b2659c4ceb0d0f86c433b905281f9f9a69ee
kibebr <0763722@students.dadeschools.net>
2021-03-19 23:57:44 +0000

    unused fonts are not being fetched anymoreg

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -32,8 +32,6 @@ export default class MyDocument extends Document {
         <Head>
           <meta name="viewport" content="initial-scale=1.0, width=device-width" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link href="https://fonts.googleapis.com/css2?family=Vollkorn:wght@500;600&display=swap" rel="stylesheet" />
-          <link href="https://fonts.googleapis.com/css2?family=Libre+Baskerville&family=Libre+Franklin&display=swap" rel="stylesheet" />
           <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@300;400;500;600&display=swap" rel="stylesheet" />
           <link href="https://fonts.googleapis.com/css2?family=Domine:wght@400;500;600&display=swap" rel="stylesheet" />
           <title>Novas</title>
