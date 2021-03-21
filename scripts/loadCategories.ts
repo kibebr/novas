ee771afbaabbd6861d29c21b1a3e33975e820584
kibebr <0763722@students.dadeschools.net>
2021-03-21 15:20:42 +0000

    fixed script to detect and report an error if there is any; build script now clears tmp directories and makes new ones

--- scripts/loadCategories.ts
+++ scripts/loadCategories.ts
@@ -1,20 +1,23 @@
 /* eslint-disable @typescript-eslint/no-floating-promises */
 
-import { save } from "../utils/File";
-import { getCategoriesWithArticles } from "../fetchers/newsapi/index";
+import { save } from '../utils/File'
+import { getCategoriesWithArticles } from '../fetchers/newsapi/index'
 
 (async () => {
-  const categories = await getCategoriesWithArticles()();
+  const categories = await getCategoriesWithArticles()()
 
-  if (categories._tag === "Left") {
-    console.error("Error while trying to fetch categories:");
-    throw new Error(categories.left.message);
+  if (categories._tag === 'Left') {
+    console.error('Error while trying to fetch categories:')
+    throw new Error(categories.left.message)
   } else {
-    const stringified = JSON.stringify(categories.right);
-    const res = await save("./.tmp/categories.json", stringified)();
-    if (res instanceof Error) {
-      console.error("Error while trying to save categories:");
-      throw new Error(res.message);
+    const stringified = JSON.stringify(categories.right)
+    const res = await save('./.tmp/categories.json', stringified)()
+
+    if (res._tag === 'Left') {
+      console.error('Error while trying to save categories:')
+      throw new Error(res.left.message)
+    } else {
+      console.log('Stored categories.')
     }
   }
-})();
+})()
