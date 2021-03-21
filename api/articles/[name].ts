b3f59e50590ddcd32193bb0b3794b1fef38624af
kibebr <0763722@students.dadeschools.net>
2021-03-21 18:26:36 +0000

    added clear cache script

--- api/articles/[name].ts
+++ api/articles/[name].ts
@@ -26,19 +26,19 @@ const Response = t.record(t.string, CategoryV)
 
 type Article = t.TypeOf<typeof ArticleV>
 
-const filterWithTitle = (t: string): (xs: Article[]) => Article[] => F.flow(
-  A.filter(F.flow(
+const filterWithTitle = (t: string): (xs: Article[]) => Article[] => A.filter(
+  F.flow(
     R.prop('title'),
     STDS.toLower,
     STDS.contains(F.pipe(t, STDS.toLower))
-  ))
+  )
 )
 
 export default async (req: VercelRequest, res: VercelResponse): Promise<void> => {
   const { name } = req.query
 
   const findArticle = F.pipe(
-    join(__dirname, '../../.tmp', 'categories.json'),
+    join(__dirname, 'categories.json'),
     read,
     TE.map(asString),
     TE.chainEitherKW(F.flow(J.parse, E.chainW(Response.decode))),
